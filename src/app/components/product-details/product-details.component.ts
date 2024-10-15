57225f52a0306ffe0a3e308d6cb966b5b1754339
Alfredo VM <alfredvillauaslp@gmail.com>
2024-10-15 00:36:44 -0600

    Perfeccionamiento de Detalles

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,17 +1,19 @@
 import { Component, inject, Input } from '@angular/core';
 import { Product } from '../../interfaces/product.interface';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
   private productsService = inject(ProductsService);
+  private router = inject(Router);
   product!: Product;
 
   constructor(
@@ -25,8 +27,23 @@ export class ProductDetailsComponent {
     });
   }
 
+  goBack(): void{
+    this.router.navigate(['/products']);
+  }
+
   getFormattedPrice() {
     const [integerPart, decimalPart] = this.product.price.toFixed(2).split('.');
     return { integerPart, decimalPart };
+  }
+
+  getFullStars(): any[] {
+    const fullStars = Math.floor(this.product.rating.rate);
+    return new Array(fullStars);
+  }
+
+  getEmptyStars(): any[] {
+    const fullStars = Math.floor(this.product.rating.rate);
+    const emptyStars = 5 - fullStars;
+    return new Array(emptyStars);
   }
 }
